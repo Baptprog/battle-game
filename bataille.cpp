adfdbd5a1a33eef42c8ff54bc353231806ed777a
Baptprog <111776453+Baptprog@users.noreply.github.com>
2024-04-06 21:44:43 +0200

    Add files via upload

---
+++ bataille.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+void bataille(){
+	srand(time(0));
+	vector<int> cartes={2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14};
+	vector<int> c1={}; // players 1 cards
+	vector<int> c2={};
+	
+	for (int i=0;i<=cartes.size()/2;i++){
+		auto it=cartes.begin();
+		int n=rand()%cartes.size();
+		c1.push_back(cartes[n]);
+		advance(it,n); // Define the indice 'n'
+		cartes.erase(it); // Pop cartes[n]
+	}
+	for (int i=0;i<=cartes.size()/2;i++){
+		int n=rand()%cartes.size();
+		auto it=cartes.begin();
+		c2.push_back(cartes[n]);
+		advance(it,n);
+		cartes.erase(it);
+	}
+	
+	//Beginning of the game
+	
+	while (!c1.empty() && !c2.empty()){
+		if (c1[0]>c2[0]){
+			auto it=c2.begin();
+			c1.push_back(c2[0]);
+			advance(it,0);
+			c2.erase(it);
+			cout<<"Le joueur 1 remporte le pli avec "<<c1[0]<<" contre "<<c2[0]<<endl;
+		}
+		else if(c1[0]<c2[0]){
+			auto it=c1.begin();
+			c2.push_back(c2[0]);
+			advance(it,0);
+			c1.erase(it);
+			cout<<"Le joueur 2 remporte le pli avec "<<c2[0]<<" contre "<<c1[0]<<endl;
+		}
+		else if(c1[0]==c2[0]){
+			while (c1[0] == c2[0]) {
+                		int n1 = rand() % c1.size();
+                		int n2 = rand() % c2.size();
+                		swap(c1[n1], c2[n2]); // Waiting while two cards are same
+            	}
+		}
+	}
+	if(c1.empty()){
+		cout<<"Le joueur 2 a gagne la partie"<<endl;
+	}
+	else if(c2.empty()){
+		cout<<"Le joueur 1 a gagne la partie"<<endl;
+	}
+}
+
+int main(){
+	bataille();
+}
